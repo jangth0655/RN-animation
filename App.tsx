a629f1cc666a75f50b6faa2b8454baa3ee841dbe
jangth0655 <jangth0655@gmail.com>
2023-06-06 23:36:31 +0900

    animate - penResponder

--- App.tsx
+++ App.tsx
@@ -1,35 +1,81 @@
-import {useState} from 'react';
+import {Animated, PanResponder, Platform, StyleSheet, View} from 'react-native';
 import styled from 'styled-components/native';
+import PizzaIcon from 'react-native-vector-icons/Ionicons';
+import {useRef} from 'react';
 
 function App() {
-  const [y, setY] = useState(0);
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, {dx, dy}) => {
+        position.setValue(dx);
+      },
+      onPanResponderGrant: () => onPressIn(),
+      onPanResponderRelease: () => {
+        Animated.parallel([
+          onPressOut,
+          Animated.spring(position, {toValue: 0, useNativeDriver: true}),
+        ]).start();
+      },
+    }),
+  ).current;
 
-  const moveUp = () => {
-    setInterval(() => setY(prev => prev + 1), 500);
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+
+  const onPressIn = () => {
+    Animated.spring(scale, {
+      toValue: 0.95,
+      useNativeDriver: true,
+    }).start();
   };
+
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
 
   return (
     <Container>
-      <Box
-        onPress={moveUp}
-        style={{
-          transform: [{translateY: y}],
-        }}
-      />
+      <Card
+        {...panResponder.panHandlers}
+        style={[
+          styles.animateCard,
+          {transform: [{scale}, {translateX: position}]},
+        ]}>
+        <PizzaIcon name="pizza" color="#192a56" size={98} />
+      </Card>
     </Container>
   );
 }
+
+export default App;
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: #00a8ff;
 `;
 
-const Box = styled.Pressable`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
+const Card = styled(Animated.createAnimatedComponent(View))`
+  width: 300px;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  background-color: #fff;
 `;
 
-export default App;
+const styles = StyleSheet.create({
+  animateCard: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
