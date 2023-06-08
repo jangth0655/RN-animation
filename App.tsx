1835c57fe5d8b96737003a4a6ebd597ca0f5d265
jangth0655 <jangth0655@gmail.com>
2023-06-08 23:19:45 +0900

    card animation

--- App.tsx
+++ App.tsx
@@ -1,27 +1,60 @@
-import {Animated, PanResponder, Platform, StyleSheet, View} from 'react-native';
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
 import styled from 'styled-components/native';
-import PizzaIcon from 'react-native-vector-icons/Ionicons';
-import {useRef} from 'react';
+import FoodIcon from 'react-native-vector-icons/Ionicons';
+import ButtonIcon from 'react-native-vector-icons/Ionicons';
+import {useRef, useState} from 'react';
+import {icons} from './icons';
 
 function App() {
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ['-15deg', '15deg'],
+    extrapolate: 'clamp',
+  });
+
+  const secondScale = position.interpolate({
+    inputRange: [-280, 0, 280],
+    outputRange: [1, 0.7, 1],
+    extrapolate: 'clamp',
+  });
+
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (_, {dx, dy}) => {
+      onPanResponderMove: (_, {dx}) => {
         position.setValue(dx);
       },
       onPanResponderGrant: () => onPressIn(),
-      onPanResponderRelease: () => {
-        Animated.parallel([
-          onPressOut,
-          Animated.spring(position, {toValue: 0, useNativeDriver: true}),
-        ]).start();
+      onPanResponderRelease: (_, {dx}) => {
+        if (dx < -250) {
+          goLeft.start();
+        } else if (dx > 250) {
+          goRight.start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     }),
   ).current;
 
-  const scale = useRef(new Animated.Value(1)).current;
-  const position = useRef(new Animated.Value(0)).current;
+  const goLeft = Animated.spring(position, {
+    toValue: -500,
+    tension: 5,
+    useNativeDriver: true,
+  });
+
+  const goRight = Animated.spring(position, {
+    toValue: 500,
+    tension: 5,
+    useNativeDriver: true,
+  });
+
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
 
   const onPressIn = () => {
     Animated.spring(scale, {
@@ -35,16 +68,45 @@ function App() {
     useNativeDriver: true,
   });
 
+  const closePress = () => {
+    goLeft.start(onDismiss);
+  };
+
+  const checkPress = () => {
+    goRight.start(onDismiss);
+  };
+
+  const [index, setIndex] = useState(0);
+
+  const onDismiss = () => {
+    scale.setValue(1);
+    position.setValue(0);
+    setIndex(prev => prev + 1);
+  };
+
   return (
     <Container>
-      <Card
-        {...panResponder.panHandlers}
-        style={[
-          styles.animateCard,
-          {transform: [{scale}, {translateX: position}]},
-        ]}>
-        <PizzaIcon name="pizza" color="#192a56" size={98} />
-      </Card>
+      <CardContainer>
+        <Card style={[styles.animateCard, {transform: [{scale: secondScale}]}]}>
+          <FoodIcon name={icons[index + 1]} color="#192a56" size={98} />
+        </Card>
+        <Card
+          {...panResponder.panHandlers}
+          style={[
+            styles.animateCard,
+            {transform: [{scale}, {translateX: position}, {rotateZ: rotation}]},
+          ]}>
+          <FoodIcon name={icons[index]} color="#192a56" size={98} />
+        </Card>
+      </CardContainer>
+      <ButtonContainer>
+        <Button onPress={closePress}>
+          <ButtonIcon name="close-circle" color={'#fff'} size={46} />
+        </Button>
+        <Button onPress={checkPress}>
+          <ButtonIcon name="checkmark-circle" color={'#fff'} size={46} />
+        </Button>
+      </ButtonContainer>
     </Container>
   );
 }
@@ -58,13 +120,30 @@ const Container = styled.View`
   background-color: #00a8ff;
 `;
 
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Card = styled(Animated.createAnimatedComponent(View))`
-  width: 300px;
-  height: 300px;
+  width: 150px;
+  height: 150px;
   justify-content: center;
   align-items: center;
   border-radius: 12px;
   background-color: #fff;
+  margin-bottom: 20px;
+  position: absolute;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const Button = styled.Pressable`
+  margin: 0px 10px;
 `;
 
 const styles = StyleSheet.create({
---
+++ icons.ts
@@ -0,0 +1,1 @@
+export const icons = ['link', 'search', 'pizza', 'beer'];
